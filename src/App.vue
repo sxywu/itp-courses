ee6929f679ddea1a23d382e5bda4f3790a103e3d
Shirley Wu <shirleyxueyangwu@gmail.com>
2019-10-09 23:22:34 -0400

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div id='header'>
+      <h1>Exploring Forty Years of ITP Classes</h1>
+    </div>
+    <div id='byline'>
+      data by Jim Schmitz // visualization by Shirley Wu
+    </div>
+
     <Galaxy v-if='!selectedGalaxy' v-for='galaxy in galaxies' v-bind='{galaxy}' />
     <Detail v-if='selectedGalaxy' />
 
@@ -42,6 +49,19 @@ export default {
 </script>
 
 <style scoped>
+#app {
+  margin-top: 20px;
+}
+
+#header {
+  font-size: 24px;
+  text-align: center;
+}
+
+#byline {
+  text-align: center;
+}
+
 .hovered {
   position: absolute;
   background: #fff;
